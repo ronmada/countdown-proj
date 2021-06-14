0fcc2e07d75859c357e5757065f7231a66d25a25
Ron Wexler <ronmada@gmail.com>
2021-06-14 21:00:08 +0300

    v0.5

--- src/app/timer/timer.page.ts
+++ src/app/timer/timer.page.ts
@@ -8,33 +8,35 @@ import { Timer } from './timer';
 export class TimerPage implements OnInit {
   public changedNum: number;
   public timer: Timer;
-
+  private timeInterval: ReturnType<typeof setInterval>;
   constructor() {}
 
-  ngOnInit() {
-    this.calcTimer();
+  public ngOnInit(): void {
+    if (this?.timer) {
+      return;
+    } else {
+      this.calcTimer();
+    }
   }
   public btnClick(): void {
-    this.calcTimer(true);
+    clearInterval(this.timeInterval);
+    this.calcTimer();
   }
 
-  private calcTimer(clear?: boolean): void {
-    // if (this?.timer) {
-    //   return;
-    // }
-    const max = 10 ** 12;
+  private calcTimer(): void {
+    const max = 10 ** 11;
     const minimum = 10000;
     const initNumber =
       Math.floor(Math.random() * (max - minimum + 1)) + minimum;
     let changednum = initNumber;
     this.changedNum = initNumber;
-    console.log(initNumber);
-    const timeInterval = setInterval(() => {
-      if (changednum === 0 || clear) {
-        clearInterval(timeInterval);
+    console.log(`${initNumber} in milliseconds`);
+    this.timeInterval = setInterval(() => {
+      if (changednum === 0) {
+        clearInterval(this.timeInterval);
       }
       this.changedNum = changednum;
-      console.log(changednum);
+      // console.log(changednum);
       const sec = Math.floor((changednum % (1000 * 60)) / 1000);
       const min = Math.floor((changednum % (1000 * 60 * 60)) / (1000 * 60));
       const hrs = Math.floor(
